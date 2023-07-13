674ee6296dbc630cbb0497059dda9e5e6d28560d
John-Livingproof Fowowe <johnlivingprooff@gmail.com>
2023-07-13 13:25:09 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 /*
  *This code prints out a string with the puts function
  *This is the entry point of the c program
- */
-int main(void)
+ */int main(void)
 {
 	char aStr[] = "\"Programming is like building a multilingual puzzle";
 
