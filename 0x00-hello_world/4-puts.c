4bc21ec40f9c7d3c5a0be63be9ddfac89db132f5
John-Livingproof Fowowe <johnlivingprooff@gmail.com>
2023-07-13 13:10:52 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char *aStr = ""Programming is like building a multilingual puzzle";
+	char aStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(aStr);
 	return (0);
