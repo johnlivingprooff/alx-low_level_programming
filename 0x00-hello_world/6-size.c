85838d6d639ae945df3498ece0597ab366413b9f
John-Livingproof Fowowe <johnlivingprooff@gmail.com>
2023-07-13 15:07:14 -0700

    Used the old

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,7 @@
  * main - Entry point
  *
  * Return: void
- */
-int main(void)
+ */int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
