2ffee3a54b291a8652e025246ba1721dc2c829b4
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-15 01:34:31 +0200

    0x01-variables_if_else_while project

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,11 @@
 		n = 1;
 		while (n <= 9)
 		{
-			putchar(i + '0');
-			putchar(n + '0');
+			if (i != 1 && n != 1)
+			{
+				putchar(i + '0');
+				putchar(n + '0');
+			}
 			if (n != 9)
 			{
 				putchar(',');
