8f34c72960b276bf26f86ded56b13eaa83aeb0fa
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-14 20:02:13 +0200

    0x01-variables_if_else_while project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ *main - Entry Point
+ *
+ *returns 0
+ *
+ */int main(void)
+{
+	char lowerCase[26];
+	char upperCase[26];
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		lowerCase[i] = 'a' + i;
+		putchar(lowerCase[i]);
+		upperCase[i] = 'A' + i
+		putchar(upperCase[i]);
+	}
+
+		putchar('\n');
+
+	return (0);
+}
