520503b24916ced378026393bf2caa411d8ee92b
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-14 21:57:24 +0200

    0x01-variables_if_else_while project

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ *main - Entry Point
+ *
+ *returns 0
+ *
+ */int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i++)
+	{
+		putchar(i);
+	}
+
+		putchar(10);
+
+	return (0);
+}
