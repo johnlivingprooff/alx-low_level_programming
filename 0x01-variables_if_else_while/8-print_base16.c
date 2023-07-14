14d5bd4b4ddc40f37e8a82ccb8813a4031ea8775
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-14 22:08:14 +0200

    0x01-variables_if_else_while project

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ *main - Entry Point
+ *
+ *returns 0
+ *
+ */int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	for (i = 0; 10 <= i < 17; i++)
+	{
+		putchar(65 + i);
+	}
+
+	putchar('\n');
+	return (0);
+}
