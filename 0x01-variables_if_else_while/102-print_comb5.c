a3bbb2cc1ffa199c67f8155065b348a71ea2949e
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-15 03:51:38 +0200

    0x01-variables_if_else_while project

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+ *main - Entry Point
+ *
+ *returns 0
+ *
+ */int main(void)
+{
+	int a = 0;
+	int b = 1;
+	for (; a <= 99; a++)
+	{
+		while (b <= 99)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (a != 99 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		b = a + 2;
+	}
+	putchar('\n');
+	return (0);
+}
