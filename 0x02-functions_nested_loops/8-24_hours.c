0a434d008346906b330fdb354304cb542ad72a61
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-18 22:49:03 +0200

    0x02-functions_nested_loops project

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * Function that prints the last digit of a number.
+ */void jack_bauer(void)
+{
+	int i, n;
+
+	for (i = 0; i <= 23; i++)
+	{
+		n = 0;
+		for (n = i + 1; n <= 59; n++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(10);
+		}
+	}
+}
+
