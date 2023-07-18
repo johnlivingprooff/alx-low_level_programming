9594446744dc83923eafed47a2ef9b6cc64a69f6
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-19 01:11:33 +0200

    0x02-functions_nested_loops project

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,31 +7,13 @@
  *
  */void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
+	for (; n <= 98; n++)
 	{
-		for (i = n; i <= 98; i++)
+		printf("%d", n)
+		if (n != 98)
 		{
-			printf("%d", i);
-			if (i <= 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf(", ");
 		}
-		_putchar(10);
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-		}
-		_putchar(10);
 	}
 }
