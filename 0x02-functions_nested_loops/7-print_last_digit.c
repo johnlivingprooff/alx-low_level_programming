642ebb19393020d29795ac1a84b4c9ad20fa7e7c
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-18 22:26:57 +0200

    0x02-functions_nested_loops project

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,19 +4,17 @@
  * Function that prints the last digit of a number.
  */int print_last_digit(int x)
 {
+	int i = (x % 10);
+
+	_putchar(i + '0');
+
 	if (x < 0)
 	{
 		int i = (x % 10) * -1;
 
 		_putchar(i + '0');
 	}
-	else
-	{
-		int i = (x % 10);
 
-		_putchar(i + '0');
-
-	}
 
 	return (i);
 }
