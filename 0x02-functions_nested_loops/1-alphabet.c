8dfa13340bf8eb054b1bc8fc608482035ba32d23
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-18 18:33:16 +0200

    0x02-functions_nested_loops project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,38 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-
-/*
- * main Function - Entry
- *
- * prints the alphabet, in lowercase
- * followed by a new line.
- *
- */int main(void)
-{
-	char lCase[26];
-	int i;
-
-	print_alphabet();
-
-	for (i = 0; i < 26; i++)
-	{
-		_putchar(lCase[i]);
-	}
-	_putchar('\n');
-
-	return (0);
-}
 
 /*
  * Function that prints lowercase alphabets
  *
- */void print_alphabet()
+ */void print_alphabet(void)
 {
 	char l;
 
 	for (l = 97; l <= 122; l++)
 	{
-		putchar(l);
+		_putchar(l);
 	}
-	putchar(10);
+	_putchar(10);
 }
