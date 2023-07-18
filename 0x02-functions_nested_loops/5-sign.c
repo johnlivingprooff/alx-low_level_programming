6540e62435136154513176661c7697e76b8da7ad
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-18 20:23:45 +0200

    0x02-functions_nested_loops project

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * function that  checks for lowercase character.
+ *
+ */int _isalpha(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
