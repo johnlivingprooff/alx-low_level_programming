970c1f2a1219fa1abc54a6ead29acce5f5d1ffd2
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-18 18:54:20 +0200

    0x02-functions_nested_loops project

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * function that prints 10 times the alphabet
+ * in lowercase, followed by a new line.
+ *
+ */void print_alphabet_x10(void)
+{
+	int i;
+	char l;
+	for (i = 0; i < 10; i++)
+	{
+		for (l = 97; l != 123; l++)
+		{
+			_putchar(l);
+		}
+		_putchar(10);
+	}
+}
