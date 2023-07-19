d707d2cfcc9e580b86bba072045be1cb9c322922
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-19 02:14:20 +0200

    0x02-functions_nested_loops project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,14 +12,7 @@
 		for (n = 0; n <= 9; n++)
 		{
 			prod = i * n;
-			if (i == 9)
-			{
-				printf("%2d", prod);
-			}
-			else
-			{
-				printf("%2d, ", prod);
-			}
+			printf("%2d, ", prod)
 		}
 		printf("\n");
 	}
