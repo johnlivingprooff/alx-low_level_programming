6efc2faa463872be9ac870d1dcb2ca545f2d6314
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-19 20:31:42 +0200

    second chance for 0x02 project

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@
 				{
 					/*printf("%3d", prod);*/
 					_putchar(prod / 100 + '0');
-					_putchar(prod / 10 + '0');
+					_putchar(prod / 10 % 10 + '0');
 					_putchar(prod % 10 + '0');
 				}
 				else if (x == 0)
