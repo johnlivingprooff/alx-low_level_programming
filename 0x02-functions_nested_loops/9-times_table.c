8bb58959c45158b0ba36dc110335da5844333b82
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-19 02:24:36 +0200

    0x02-functions_nested_loops project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  * Function that prints the last digit of a number.
  */void times_table(void)
 {
-	int div, i, n, prod;
+	int i, n, prod;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -14,12 +14,11 @@
 			prod = i * n;
 			printf("%2d, ", prod);
 		}
-		div = prod / n;
-		if (div == 9)
-		{
-			printf("%2d", prod);
-		}
 		printf("\n");
+		if (i == 9)
+		{
+			printf("%2d", i * n)
+		}
 	}
 }
 
