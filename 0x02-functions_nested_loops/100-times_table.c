57d678e05f8f4dd9954e3b2acbc9f3d35e5506e3
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-19 12:12:38 +0200

    This is a cp of tsk 9 -- tsk 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * function that prints the n times table, starting with 0
+ */void print_times_table(int n)
+{
+	int i, x, prod;
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			for (x = 0; x <= 9; x++)
+			{
+				prod = i * x;
+				if (x == n)
+				{
+					printf("%2d", prod);
+				}
+				else if (x == 0)
+				{
+					printf("%d, ", prod);
+				}
+				else
+				{
+					printf("%2d, ", prod);
+				}
+			}
+			printf("\n");
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
