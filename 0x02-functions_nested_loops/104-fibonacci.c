b63774505d5652c41b670bf56c57ea327bbc1c89
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-20 00:38:59 +0200

    second chance for 0x02 project

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,11 +7,10 @@
  */int main(void)
 {
 	int i;
-	unsigned long fib1 = 1, fib2 = 2, next;
+	unsigned long fib1 = 0, fib2 = 1, next;
 	unsigned long f1h1, f2h1, f1h2, f2h2, half1, half2;
-	
-	printf("1, 2, ");
-	for (i = 0; i < 92; i++)
+
+	for (i = 0; i < 92; ++i)
 	{
 	next = fib1 + fib2;
 	printf("%lu, ", next);
@@ -22,7 +21,7 @@
 	f2h1 = fib2 / 10000000000;
 	f1h2 = fib1 % 10000000000;
 	f2h2 = fib2 % 10000000000;
-	for (i = 92; i < 99; i++)
+	for (i = 93; i < 99; i++)
 	{
 	half1 = f1h1 + f2h1;
 	half2 = f1h2 + f2h2;
