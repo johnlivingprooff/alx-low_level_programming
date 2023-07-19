2c0102f003349e236f0a58d62413604ce49e9405
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-19 15:07:01 +0200

    second chance for 0x02 project

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
-int fibon(int n);
+void fibon(int n);
 /*
- *
+ * main - Entry point
  *
  *
  */int main(void)
 {
-	int n;
+	int numbers = 50;
 	
-	fibon(n)
-	for (; n)
-	printf()
+	fibon(numbers);
+
+	return (0);
+}
+/*
+ * Function creates a fibonacci sequence
+ * the First 50
+ *
+ */void fibon(int n)
+{
+	int term1 = 1, term2 = 2;
+	int next;
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d, ");
+
+		next = term1 + term2;
+		term1 = term2;
+		term2 = next;
+	}
 }
