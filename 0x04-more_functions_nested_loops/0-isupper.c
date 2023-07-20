98bc62d75344525b788b23210a02bc643e114b57
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-20 16:02:45 +0200

    0x04-more_functions_nested_loops project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* Desc: function that checks
+* for uppercase character.
+*
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* All the Function prototypes for this project */
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
