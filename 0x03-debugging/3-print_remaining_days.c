ecf2fb822e60e99c6c54e3a59d3ea1d8ccfdb5dc
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-20 14:04:17 +0200

    0x03-debbuging project

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,25 +11,23 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	int leapYear = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+	int m, totalDays, remDays;
+	int mth[];
+
+	mth[] = {0, 31, leapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	if (month < 1 || month > 12 || day < 1 || day > 31)
 	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-			printf("Day of the year: %d\n", day);
-		}
-		printf("Remaining days: %d\n", 366 - day);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 	}
 	else
 	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %2d/%2d/%4d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %2d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+		totalDays = day;
+		for (m = 1; m < month; m++)
+			totalDays += mth[m]
+		remDays = leapYear ? 366 - totalDays : 365 - totalDays;
+
+		 printf("Day of the year: %d\n", totalDays);
+		 printf("Remaining days: %d\n", remDays);
 	}
 }
