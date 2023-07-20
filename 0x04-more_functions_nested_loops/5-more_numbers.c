d79f02975050699c8eb29dfef8c172eb8ed94882
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-20 17:57:33 +0200

    0x04-more_functions_nested_loops project

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* more_numbers - prints more numbers
+* @void
+* Return: no
+*/
+void more_numbers(void)
+{
+	int i, a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			
+			_putchar(a % 10 + '0');
+		}
+		_putchar(10);
+	}
+}
