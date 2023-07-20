54a592608d0a8a4d411fbc160edc8d50b4aadf92
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-20 11:40:03 +0200

    0x03-debbuging project

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60)
 		{
-		day++;
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
@@ -25,11 +25,11 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+			printf("Invalid date: %2d/%2d/%4d\n", month, day - 31, year);
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
+			printf("Day of the year: %2d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
