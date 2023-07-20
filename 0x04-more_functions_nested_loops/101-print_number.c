a077c73356397de7ef96ef2147021da9925c0933
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-21 01:42:31 +0200

    number_control copy 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,51 @@
 #include "main.h"
 
+/**
+* print_number - Entry point
+* @n: prints integers
+* Return:
+*/
 void print_number(int n)
 {
-    int count = 0, digit, i, tmp, an;
-    int div = 1;
-    int absolute_n = n < 0 ? -n : n;
+int count = 0, digit, i, tmp, an;
+int div = 1;
+int absolute_n = n < 0 ? -n : n;
 
-    /* handles output when n is 0 */
-    if (absolute_n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+/* handles output when n is 0 */
+if (absolute_n == 0)
+{
+_putchar('0');
+return;
+}
 
-    /* to count the digits in 'n' */
-    tmp = absolute_n;
-    while (tmp != 0)
-    {
-        tmp /= 10;
-        count++;
-    }
+/* to count the digits in 'n' */
+tmp = absolute_n;
+while (tmp != 0)
+{
+tmp /= 10;
+count++;
+}
 
-    /* for negative n */
-    if (n < 0)
-    {
-        _putchar('-');
-    }
+/* for negative n */
+if (n < 0)
+{
+_putchar('-');
+}
 
-    /* what is Div? */
-    for (i = 1; i < count; i++)
-    {
-        div *= 10;
-    }
+/* what is Div? */
+for (i = 1; i < count; i++)
+{
+div *= 10;
+}
 
-    /* extracts & prints each digit */
-    an = absolute_n;
-    for (i = count; i >= 1; i--)
-    {
-        digit = an / div;
-        _putchar(digit + '0');
-        an = an % div; /* removes left digit */
-        div /= 10;
-    }
+/* extracts & prints each digit */
+an = absolute_n;
+for (i = count; i >= 1; i--)
+{
+digit = an / div;
+_putchar(digit + '0');
+an = an % div; /* removes left digit */
+div /= 10;
+}
 }
 
