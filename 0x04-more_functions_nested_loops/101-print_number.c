5e9de832e64cfc1db6727d943a537926d8c0c81f
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-21 01:58:10 +0200

    reverted to old code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,51 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to print
- * Return: void
- */
+* print_number - Entry point
+* @n: prints integers
+* Return:
+*/
 void print_number(int n)
 {
-	int count, digit, i;
-	int div = 10;
+int count = 0, digit, i, tmp, an;
+int div = 1;
+int absolute_n = n < 0 ? -n : n;
 
-	while (n != 0)
-	{
-		n = n / 10;
-		count++;
-	}
-
-	if (n < 0)
-		_putchar('-');
-
-	for (i = count; i >= 0; i--)
-	{
-		digit = n / div;
-		_putchar(digit + '0');
-	}
+/* handles output when n is 0 */
+if (absolute_n == 0)
+{
+_putchar('0');
+return;
 }
+
+/* to count the digits in 'n' */
+tmp = absolute_n;
+while (tmp != 0)
+{
+tmp /= 10;
+count++;
+}
+
+/* for negative n */
+if (n < 0)
+{
+_putchar('-');
+}
+
+/* what is Div? */
+for (i = 1; i < count; i++)
+{
+div *= 10;
+}
+
+/* extracts & prints each digit */
+an = absolute_n;
+for (i = count; i >= 1; i--)
+{
+digit = an / div;
+_putchar(digit + '0');
+an = an % div; /* removes left digit */
+div /= 10;
+}
+}
+
