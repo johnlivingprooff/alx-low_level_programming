a2637b324347ba55f7261246667bff4549bdbd22
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-20 18:15:13 +0200

    0x04-more_functions_nested_loops project

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_line - draws a straight line in the terminal.
+* @n: draws the line
+* Return: nnthing returned
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 0;
+		while (i <= n)
+		{
+			i++;
+			_putchar(95);
+		}
+
+		_putchar(10);
+	}
+	else if (n <= 0)
+		_putchar(10);
+}
