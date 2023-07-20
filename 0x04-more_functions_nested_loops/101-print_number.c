2accbe74e8f992e0635b7c210d90d7f6d375285b
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-20 23:44:37 +0200

    0x04-more_functions_nested_loops project

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+* print_number - Entry point
+* @n: prints integers
+* Return:
+*/
+void print_number(int n)
+{
+	int count = 0, digit, i, temp;
+	int div = 1;
+
+	/* handles output when n is 0 */
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+
+	/* to count the digits in 'n' */
+	temp = n;
+	while (temp != 0)
+	{
+		temp /= 10;
+		count++;
+	}
+
+	/* for negative n */
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	
+	/* what is Div? */
+	for (i = 1; i < count; i++)
+	{
+		div *= 10;
+	}
+
+	/* extracts & prints each digit */
+	for (i = count; i >= 1; i--)
+	{
+		digit = n / div;
+		_putchar(digit + '0');
+		n = n % div; /* removes left digit */
+		div /= 10;
+	}
+}
