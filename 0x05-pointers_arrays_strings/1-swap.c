2d29b1eef3274f68e9feb2874363285898444163
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-24 19:34:55 +0200

    0x05-pointers_arrays_strings project

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,11 +7,6 @@
 */
 void swap_int(int *a, int *b)
 {
-	int *c;
-
-	a = b;
-	b = c;
-	printf("is a's address still %p\n", a);
-	printf("is b's address still %p\n", b);
-	printf("what is c's address %p\n", c);
+	*a = 42;
+	*b = 98;
 }
