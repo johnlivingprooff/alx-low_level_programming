dc1de84aac5de3036418e9024f505c8ada7ddcfd
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-24 23:29:44 +0200

    0x05-pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: pointer that prints string in reverse
+* Return: void
+*/
+void rev_string(char *s)
+{
+	int length;
+
+	length = _strlen(s);
+	/* printf("lenght of string: %d\n", length); */
+	while (length > 0)
+	{
+		_putchar(s[length - 1]);
+		length--;
+	}
+	_putchar(10);
+}
