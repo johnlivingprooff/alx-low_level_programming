12c07c9cb1b47f3dc9fc6c4d484610685f48b0ec
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-24 19:43:17 +0200

    0x05-pointers_arrays_strings project

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-* swap - Function swaps the value of two integers
-* @*a: swaps into &b && @*b: swaps into &a
+* swap_int - Function swaps the value of two integers
+* @a: swaps into &b
+* @b: swaps into &a
 * Return: void
 */
 void swap_int(int *a, int *b)
 {
 	int n;
-	
-	n = *a; 
+
+	n = *a;
 	*a = *b;
 	*b = n;
 }
