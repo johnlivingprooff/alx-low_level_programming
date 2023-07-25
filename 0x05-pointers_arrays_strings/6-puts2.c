cf5551d66ba082b73eb7f0d7f78109ef98284b53
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-25 02:16:49 +0200

    0x05-pointers_arrays_strings project

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (++i; *str != '\0'; ++str)
+	for (; str[i] != '\0'; ++i)
 	{
 		if (i % 2 == 0)
-		_putchar(*str);
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
