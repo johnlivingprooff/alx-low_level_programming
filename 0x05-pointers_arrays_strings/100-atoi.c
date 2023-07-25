3fd62d2e97ebc1a58691342b2eaeba0a8fc8bfd5
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-25 04:14:04 +0200

    0x05-pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _atoi - Function converts a string to an integer
+* @s: converts the string
+* Return: void
+*/
+int _atoi(char *s)
+{
+	int i, n = 0;
+
+	for (i = 0; s[i] >= '0' && s[i] <= '9')
+		n = 10 * n + (s[i] - '0');
+	return (n);
+}
