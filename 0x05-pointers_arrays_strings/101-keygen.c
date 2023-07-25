bc29b6578cac85ece359fec64a47bb3d47649009
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-25 05:35:49 +0200

    0x05-pointers_arrays_strings project

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PWD_L 15
+
+/**
+* main - Cracks program password
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	const char typeset[95];
+	int a, i, x;
+	char password[PWD_L + 1];
+
+
+	srand(time(NULL));
+	for (a = 33; a <= 126; a++)
+		putchar(typeset[a]);
+
+	for (i = 0; i < PWD_L; i++)
+	{
+		x = rand() % (sizeof(typeset) - 1);
+		password[i] = typeset[index];
+	}
+	password[PWD_L] = '\0';
+
+	printf("%s\n", password);
+	
+	return (0);
+}
