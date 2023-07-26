471093f3b4644961bd4a7d02bb2672ca8e411d79
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-26 23:15:59 +0200

    Updated 0x06-pointers_arrays_strings project

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* strncat - function that concatenates two strings. 
+*
+* @dest: points to the address of the dest string in memory
+* @src: points to the address of the src string in memory
+* @n: amount of bytes in src
+*
+* Return: char *
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	char *tmp = dest;
+
+	for (; *dest != '\0'; dest++);
+	for (; *src != '\0'; dest++, src++)
+	{
+		if (*src < n)
+			*dest = *src;
+	}
+	return (tmp);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
 /* Functions used in this Project 0x06 */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 
 #endif
