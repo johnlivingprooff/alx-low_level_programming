e574cdd5e430db8a961470876b033bfa4e478c2a
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-27 16:46:03 +0200

    Updated 0x06-pointers_arrays_strings project

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,9 +13,9 @@ int _strcmp(char *s1, char *s2)
 	for (; *s1 != '\0' && *s2 != '\0' && *s1 == *s2; s1++, s2++)
 		;
 	if (*s1 < *s2)
-		return (-1);
+		return (-12);
 	else if (*s1 > *s2)
-		return (1);
+		return (12);
 
 	return (0);
 }
