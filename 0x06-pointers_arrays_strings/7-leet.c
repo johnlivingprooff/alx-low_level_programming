356eef54bd765567b36820d471ed4fe97ab9ffaa
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-28 01:46:48 +0200

    Updated 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* leet - function that encodes a string into 1337
+*
+* @str: string to be encoded
+*
+* Return: encoded str
+*/
+char *leet(char *str)
+{
+	int i, x;
+	char let[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	for (i = 0; *str != '\0'; str++, i++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			if (let[x] == str[i])
+				str[i] = num[x];
+		}
+	}
+	return (str);
+}
