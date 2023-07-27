c673f121121a3d8fda921e9535889ac5c4aef990
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-27 16:24:31 +0200

    Added a for-loop and more if statements :)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,10 +10,17 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 < *s2)
+	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
+	{
+		if (*s1 < *s2)
+			return (-1);
+		else if (*s1 > *s2)
+			return (1);
+	}
+	if (*s1 == '\0' && *s2 != '\0')
 		return (-1);
-	else if (*s1 > *s2)
+	else if (*s1 != '\0' && *s2 == '\0')
 		return (1);
-	else
-		return (0);
+	
+	return (0);
 }
