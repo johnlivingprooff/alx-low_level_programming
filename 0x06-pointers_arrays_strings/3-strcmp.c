c26e052b4780ee7177497ef682cc97c0dd32d71b
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-27 21:27:55 +0200

    Got help from a friend :)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 != '\0' && *s2 != '\0' && *s1 == *s2; s1++, s2++)
-		;
-	if (*s1 < *s2)
-		return (-15);
-	else if (*s1 > *s2)
-		return (15);
-
+	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+	}
 	return (0);
 }
