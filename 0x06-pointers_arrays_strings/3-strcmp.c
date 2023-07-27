4d6e3ddd3cee8d748e26902f68007b7ac055d8bc
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-27 16:42:08 +0200

    removed if arguments from for loop and switched conditions

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,12 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
-	{
-		if (*s1 < *s2)
-			return (-1);
-		else if (*s1 > *s2)
-			return (1);
-	}
-	if (*s1 == '\0' && *s2 != '\0')
+	for (; *s1 != '\0' && *s2 != '\0' && *s1 == *s2; s1++, s2++)
+		;
+	if (*s1 < *s2)
 		return (-1);
-	else if (*s1 != '\0' && *s2 == '\0')
+	else if (*s1 > *s2)
 		return (1);
-	
+
 	return (0);
 }
