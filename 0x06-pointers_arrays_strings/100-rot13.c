05938815c1e627cc1d45a6a1bf22edb50ffbfe4c
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-28 02:16:21 +0200

    Updated 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* rot13 - Ciher for characters.
+*
+* @str: point to the char that is deciphered
+*
+* Return: str
+*/
+char *rot13(char *str)
+{
+	int i, x;
+	char forwd[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char rev[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 52; x++)
+		{
+			if (forwd[x] == str[i])
+			{
+				str[i] = rev[x];
+				break;
+			}
+		}
+	}
+	return (str);
+}
