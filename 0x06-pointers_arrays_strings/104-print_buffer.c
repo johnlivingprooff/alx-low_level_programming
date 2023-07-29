f987866fb6ed7075449f5df60f2c2e6d66bce3fc
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-29 23:15:05 +0200

    Thought the deadline was tomorrow, just missed 15% extra smh

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* print_buffer - Function prints a buffer
+* Format: 00000000: xxxx xxxx xxxx xxxx (the line contained in 10 bytes)
+* @b: pointer with buffer
+* @size: for size of buffer
+*
+* Return: void
+*/
+void print_buffer(char *b, int size)
+{
+	int a, x;
+	char c;
+
+	if (size <= 0)
+	{
+		putchar(10);
+		return;
+	}
+
+	for (a = 0; a < size; a += 10)
+	{
+		printf("%08x", a);
+
+		for (x = 0; x < 10; x++)
+		{
+			if (a + x < size)
+				printf("%02x", (unsigned char)b[a + x]);
+			else
+				printf("  ");
+
+			if (x % 2 != 0)
+				printf(" ");
+		}
+
+		for (x = 0; x < 10; x++)
+		{
+			if (a + x < size)
+			{
+				c = b[a + x];
+				printf("%c", (isprint(c) && c != ' ') ? c : '.');
+			} else
+			{
+				printf(" ");
+			}
+		}
+
+		putchar(10);
+	}
+}
