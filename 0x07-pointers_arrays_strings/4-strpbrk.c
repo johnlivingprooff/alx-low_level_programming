612ffe657c6af02e17841e14e6a4eecb3fdb48b3
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-31 23:42:55 +0200

    0x07-pointers_arrays_strings project

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strpbrk -  function that searches a string for any of a set of bytes.
+*
+* @s: the input string
+* @accept: the string used in comparison
+* Return:  pointer to the byte in s that matches
+*					 one of the bytes in accept,
+*					 or NULL if no such byte is found
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	for (; *s; s++, accept++ )
+	{
+		if (*s == *accept)
+			return (s - 1);
+	}
+	return (NULL);
+}
