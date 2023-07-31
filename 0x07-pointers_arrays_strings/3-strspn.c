1848eb9290a369903ddebf8aa8d5fa3111847485
John-Livingprooff <johnlivingprooff@gmail.com>
2023-07-31 21:20:18 +0200

    0x07-pointers_arrays_strings project

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _strspn - function that gets the length of a prefix substring.
+* @accept:
+* @s: 
+* Return: unsigned int
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	
+}
