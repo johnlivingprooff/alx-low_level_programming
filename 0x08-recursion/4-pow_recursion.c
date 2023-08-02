74eaaf3650d62779babd0629576ba39979ebefb2
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-02 18:21:34 +0200

    0x08-recursion project

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _pow_recursion - function that returns
+* the value of x raised to the power of y
+* @x: number to be raised
+* @y: raised to power y
+* Return: result of x to the pow y
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (1);
+}
