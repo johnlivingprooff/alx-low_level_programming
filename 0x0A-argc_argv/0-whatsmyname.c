746d1f005ee25c053bcf8c9cd730ba749f9e673d
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-04 22:43:08 +0200

    Used printf

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,22 +8,6 @@
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	myPuts(argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
-}
-
-/**
-* _puts - that prints a string, followed by a new line
-* @s: contains the string to be printed.
-*/
-void myPuts(char *s)
-{
-	if (*s == '\0')
-	{
-		_putchar(10);
-		return;
-	}
-
-	_putchar(*s); /* this prints the first char in s */
-	myPuts(s + 1); /* recursion to iterate through s by +1 */
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 /* prototypes for 0x0A-argc_argv */
 int _putchar(char c);
 void myPuts(char *s);
