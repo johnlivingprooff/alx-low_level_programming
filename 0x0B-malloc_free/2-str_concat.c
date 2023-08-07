9a2593bfa2ff1a8a91fb6fb98f516b15f913eeed
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-07 23:14:25 +0200

    Fixed NULL trial

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,11 +11,11 @@ char *str_concat(char *s1, char *s2)
 	size_t i, j, l1, l2;
 	char *s;
 
-	if (s1 != NULL && s2 != NULL)
-	{
+	if (s1 != NULL)
 		l1 = strlen(s1);
+
+	if (s2 != NULL)
 		l2 = strlen(s2);
-	}
 
 	s = malloc((l1 + l2 + 1) * sizeof(char));
 	if (s == NULL)
