007804f4d85e8c2b72c5c2655bfa9c125c1a673f
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-07 23:03:08 +0200

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* str_concat - function that concatenates two strings.
+* @s1: string 1
+* @s2: string 2
+* Return: cat string 1 and 2, respectively
+*/
+char *str_concat(char *s1, char *s2)
+{
+	size_t i, j, l1, l2;
+	char *s;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+
+	s = malloc((l1 + l2) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < l1; i++)
+		s[i] = s1[i];
+
+	for (j = 0; j < l2; j++, i++)
+		s[i] = s2[j];
+	
+	return (s);
+}
--- 0x0B-malloc_free/tmain.c
+++ 0x0B-malloc_free/tmain.c
@@ -9,12 +9,15 @@
  */
 int main(void)
 {
-    char *s, inp[99];
+    char *s, inp1[99], inp2[99];
 
-		printf("Enter Input: ");
-		scanf("%s", inp);
+		printf("Enter Input 1: ");
+		scanf("%s", inp1);
 
-    s = _strdup(inp);
+		printf("Enter Input 2: ");
+		scanf("%s", inp2);
+    
+		s = str_concat(inp1, inp2);
     if (s == NULL)
     {
         printf("failed to allocate memory\n");
