3c6541a4e643bd0b9861d101b04cd07682a9a750
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-08 09:08:57 +0200

    I will show you how great I am - M.Ali

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,10 +2,46 @@
 
 /**
 * strtow - function that splits a string into words.
-* @str: string to be split
+* @str: string to be split by spaces
 * Return: pointer to pointer
 */
 char **strtow(char *str)
 {
-	return (**strtow(*str));
+	size_t i, j, l, w_len, w_strt = 0, index = 0;
+	char **array_of_words, *new_word;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	l = strlen(str);
+
+	array_of_words = (char **)malloc(sizeof(char *) * (l + 1))
+	if (array_of_words == NULL)
+		return (NULL);
+
+	for (i = 0; i < l; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			w_len = i - w_strt;
+
+			new_word = (char *)malloc(sizeof(char) * (w_len + 1));
+			if (new_word == NULL)
+			{
+				for (j = 0; j < index; j++)
+					free(array_of_words[j]);
+			
+			free (array_of_words);
+			return (NULL);
+			}
+		strncpy (new_word, str + w_strt, w_len);
+		new_word[w_len] = '\0';
+		array_of_words[index] = new_word;
+		index++;
+		w_strt = i + 1;
+		}
+	}
+
+	array_of_words[l] =  NULL;
+	return (array_of_words);
 }
