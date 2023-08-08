481170121ec7860862cf26a5cdcfb79db6fcd6e4
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-08 09:24:04 +0200

    pleasse!!!

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,31 +10,34 @@ char **strtow(char *str)
 	size_t i, j, l, w_len, w_strt = 0, index = 0;
 	char **array_of_words, *new_word;
 
-	if (str == NULL || str == '\0')
+	if (str == NULL || *str == '\0')
 		return (NULL);
 
 	l = strlen(str);
 
-	array_of_words = (char **)malloc(sizeof(char *) * (l + 1))
+	array_of_words = (char **)malloc(sizeof(char *) * (l + 1));
 	if (array_of_words == NULL)
 		return (NULL);
 
 	for (i = 0; i < l; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+		if (str[i] != ' ')
 		{
-			w_len = i - w_strt;
+			w_len = 0;
+
+			for (; str[i] && str[i] != ' '; i++)
+				w_len++;
 
 			new_word = (char *)malloc(sizeof(char) * (w_len + 1));
 			if (new_word == NULL)
 			{
 				for (j = 0; j < index; j++)
 					free(array_of_words[j]);
-			
-			free (array_of_words);
+
+			free(array_of_words);
 			return (NULL);
 			}
-		strncpy (new_word, str + w_strt, w_len);
+		strncpy(new_word, str + w_strt, w_len);
 		new_word[w_len] = '\0';
 		array_of_words[index] = new_word;
 		index++;
