0c658ce8cac2a843ade617ddfc63b4ea36c9118d
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-09 04:28:47 +0200

    go

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,7 +7,7 @@
 */
 char **strtow(char *str)
 {
-	size_t i, j, l = 0, w_len, w_strt = 0, index = 0;
+	size_t i, j, l = 0, w_strt = 0, index = 0, c_total = 0;
 	char **array_of_words, *new_word;
 
 	if (str == NULL || *str == '\0')
@@ -16,34 +16,32 @@ char **strtow(char *str)
 	{
 		if (str[i] != ' ' && (str[i - 1] == ' ' || i == 0))
 			l++; /* calculates no. of words */
-	}
-	array_of_words = malloc(sizeof(char *) * (l + 1));
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] != ' ')
+		if (l == 0)
+			return (NULL);
+		printf("%ld\n", l);
+		array_of_words = malloc(sizeof(char *) * (l + 1));
+		for (i = 0; str[i]; i++)
 		{
-			w_strt = i;
-			w_len = 0;
-			for (; str[i + w_len] && str[i + w_len] != ' '; w_len++)
-				;
-			if (w_len == 0)
-				return (NULL);
-			new_word = malloc(sizeof(char) * (w_len + 1));
-			if (new_word == NULL)
-			{
-				for (j = 0; j < i; j++)
-					free(array_of_words[j]);
-				free(array_of_words);
-				free(new_word);
-				return (NULL);
-			}
-		for (j = 0; j < w_len; j++)
-			new_word[j] = str[w_strt + j];
-		new_word[w_len] = '\0';
-		array_of_words[index] = new_word;
-		index++;
-		i += w_len;
+		if (str[i] != ' ')
+			c_total++;
+		/* printf("word-length: %ld\n", w_len); */
 		}
+		printf("total chars: %ld\n", c_total);
+		new_word = malloc(sizeof(char) * (c_total));
+		printf("word-start-index: %ld\n", w_strt);
+		if (new_word == NULL)
+		{
+		for (j = 0; j < c_total; j++)
+			free(array_of_words[j]);
+		free(array_of_words);
+		return (NULL);
+		}
+		for (j = 0; j < c_total; j++)
+			new_word[j] = str[w_strt + j];
+		printf("word-start-index: %ld\n", w_strt);
+		new_word[j] = '\0';
+		for (; index < l; index++)
+			array_of_words[index] = new_word;
 	}
 array_of_words[l + 1] =  NULL;
 return (array_of_words);
