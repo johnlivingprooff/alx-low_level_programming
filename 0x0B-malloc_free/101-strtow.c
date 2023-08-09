70ab0bae83047247d265e8bfb894ab4cbb3ef359
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-09 05:28:21 +0200

    total + 1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -30,13 +30,12 @@ char **strtow(char *str)
 			w_strt = i;
 			for (; str[i] && str[i] != ' '; c_total++, i++)
 				;
-			new_word = malloc(sizeof(char) * (c_total));
+			new_word = malloc(sizeof(char) * (c_total + 1));
 			if (new_word == NULL)
 			{
 				for (j = 0; j < index; j++)
 					free(array_of_words[j]);
 				free(array_of_words);
-				free(new_word);
 				return (NULL);
 			}
 			for (j = 0; j < c_total; j++)
