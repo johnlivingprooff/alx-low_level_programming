c188a31994f406e0f7463df81a19410fa4083c8f
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-09 10:52:54 +0200

    With the power of A.I.

--- 0x0B-malloc_free/other_ver/101-strtow.c
+++ 0x0B-malloc_free/other_ver/101-strtow.c
@@ -6,19 +6,19 @@
 */
 char **strtow(char *str)
 {
-	size_t i, j, l = 0, index = 0, w_len = 0;
+	size_t i, j, l = 0, w_strt = 0, index = 0, w_len = 0;
 	char **array_of_words;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		if (str[i] != ' ' && (str[i - 1] == ' ' || i == 0))
 			l++; /* calculates no. of words */
 	}
 	if (l == 0)
 		return (NULL);
-	array_of_words = (char **)malloc(sizeof(char *) * (l + 1));
+	array_of_words = malloc(sizeof(char *) * (l + 1));
 	if (array_of_words == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
@@ -26,23 +26,23 @@ char **strtow(char *str)
 		if (str[i] != ' ')
 		{
 			w_len = 0;
-			while (str[i + w_len] && str[i + w_len] != ' ')
-				w_len++;
-			array_of_words[index] = (char *)malloc(sizeof(char) * (w_len + 1));
+			w_strt = i;
+			for (; str[i] && str[i] != ' '; w_len++, i++)
+				;
+			array_of_words[index] = malloc(sizeof(char) * w_len);
 			if (array_of_words[index] == NULL)
-			{/* Cleanup and return in case of allocation failure*/
-				for (j = 0; j < index; j++)
+			{
+				for (j = 0; j < l; j++)
 					free(array_of_words[j]);
 				free(array_of_words);
 				return (NULL);
 			}
 			for (j = 0; j < w_len; j++)
-				array_of_words[index][j] = str[i + j];
-			array_of_words[index][j] = '\0'; /* Null-terminate the word*/
+				array_of_words[index][j] = str[w_strt + j];
+			array_of_words[index][j] = '\0';
 			index++;
-			i += w_len; /*Move i to the end of the current word*/
 		}
 	}
-array_of_words[index] = NULL;
-return (array_of_words);
+	array_of_words[l] =  NULL;
+	return (array_of_words);
 }
