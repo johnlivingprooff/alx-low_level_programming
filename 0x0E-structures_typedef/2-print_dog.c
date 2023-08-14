63e1faf0d132e1001ce20ba11e9d83f0a2218b52
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-14 21:34:15 +0200

    fixed up the ifs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,18 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (d->name == NULL)
-		printf("Name: (nil)");
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else if (d->name == NULL)
+		printf("Name: (nil)\n");
 
-	if (d->owner == NULL)
-		printf("Owner: (nil)");
-
-	printf("Name: %s\n", d->name);
 	if (d->age)
 		printf("Age: %f\n", d->age);
 	else
-		printf("Age: (nil)");
-	printf("Owner: %s\n", d->owner);
+		printf("Age: (nil)\n");
+
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else if (d->owner == NULL)
+		printf("Owner: (nil)\n");
 }
