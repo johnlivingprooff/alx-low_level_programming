3b14fd0d6d3b7263c0f4d964ed3c68a73375e5c7
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-14 12:47:24 +0200

    completed preprocessor project

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of this source file
+ * Return: Always 0 - success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
