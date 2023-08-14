cc50b3b876eff8419f455787342ba1a14178d670
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-14 21:18:17 +0200

    fixed 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)");
-	/**
-	 * if (d->age == 0)
-	*	printf("Age: (nil)");
-	*/
+
 	if (d->owner == NULL)
 		printf("Owner: (nil)");
 
 	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)");
 	printf("Owner: %s\n", d->owner);
 }
