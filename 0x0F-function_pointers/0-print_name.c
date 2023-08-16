e110fd5f7d4041936d39561219d3a1284c7a9f8b
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-16 15:16:57 +0200

    f(name); psh

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - points to name printing functions
+ * @name: string to be printed
+ * @f: function pointer
+ * does not return any value
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Read me file, in mark down format
