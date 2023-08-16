43f41549664f2977e65cae77ec7c5b808d1b00ed
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-16 15:33:22 +0200

    added return

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator -
+ * @array:
+ * @size:
+ * @action:
+ * doesn't return anything
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	
+}
