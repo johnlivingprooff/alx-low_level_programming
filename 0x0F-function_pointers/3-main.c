97bd2af07b61056833e6f62824d1bb28bddd52fb
John-Livingprooff <johnlivingprooff@gmail.com>
2023-08-16 20:34:01 +0200

    another push...

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
