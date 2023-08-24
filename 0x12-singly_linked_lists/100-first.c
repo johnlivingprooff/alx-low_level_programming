2f681652e6bc86359b28dd90542b310cf3d89981
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-24 22:21:32 +0200

    __attribute__((constructor))

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * prnts - this function prints
+ * everything else is void
+ */
+void __attribute__((constructor)) prnts()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
