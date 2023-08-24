7eb926ef10a0e59f4a2f1d6b4008543e8e82f580
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-25 00:47:04 +0200

    Updated 101 with -fverbose-asm -O2

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,49 @@
+	.file	"hello-world.c"
+# GNU C89 (Ubuntu 11.4.0-1ubuntu1~22.04) version 11.4.0 (x86_64-linux-gnu)
+#	compiled by GNU C version 11.4.0, GMP version 6.2.1, MPFR version 4.1.0, MPC version 1.2.1, isl version isl-0.24-GMP
+
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -mtune=generic -march=x86-64 -O2 -std=gnu90 -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection
+	.text
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"Hello, Holberton"
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+.LFB24:
+	.cfi_startproc
+	endbr64	
+	subq	$8, %rsp	#,
+	.cfi_def_cfa_offset 16
+# /usr/include/x86_64-linux-gnu/bits/stdio2.h:112:   return __printf_chk (__USE_FORTIFY_LEVEL - 1, __fmt, __va_arg_pack ());
+	leaq	.LC0(%rip), %rdi	#, tmp83
+	call	puts@PLT	#
+# hello-world.c:13: }
+	xorl	%eax, %eax	#
+	addq	$8, %rsp	#,
+	.cfi_def_cfa_offset 8
+	ret	
+	.cfi_endproc
+.LFE24:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
