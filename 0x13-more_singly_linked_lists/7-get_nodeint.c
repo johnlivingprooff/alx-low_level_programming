11e0c90f884fec40258f00454bc898c660ec6577
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-29 13:23:52 +0200

    Adding 8-Sum all

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	size_t i;
 	listint_t *tmp;
 
+	if (head == NULL)
+		return (NULL);
 	for (i = 0; i < index; i++)
 		head = head->next;
 	tmp = head;
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns (check the Return desc.)
+ * @head: the node representative
+ * Return: sum of all the data (n) of a listint_t linked list.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+		sum += head->n;
+	return (sum);
+}
