374da12cc29d78f95460981d728a1bbafa299e47
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-29 14:14:17 +0200

    update to 9th

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	current = *head;
 
 	for (i = 0; i < idx; i++)
