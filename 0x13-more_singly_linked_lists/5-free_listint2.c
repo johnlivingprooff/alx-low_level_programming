fb9cec46a42c12fe7550f6b18218bd2e2c776a37
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-30 09:24:58 +0200

    🤦🏾‍♂️🤦🏾‍♂️🤦🏾‍♂️

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		node = *head;
 		*head = (*head)->next;
 		free(node);
 	}
-	*head = NULL;
 }
