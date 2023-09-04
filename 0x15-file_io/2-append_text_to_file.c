3552b5797420961dc47b2ac892f7d0a9f4373919
johnlivingprooff <johnlivingprooff@gmail.com>
2023-09-04 19:01:14 +0200

    2. Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: pointer to the file
+ * @text_content: text to be appended
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int len, fd;
+
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		/* get the length of text */
+		len = strlen(text_content);
+		if (write(fd, text_content, len) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
