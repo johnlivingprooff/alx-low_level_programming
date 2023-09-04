1de498feb50a494565cf86f66654468f452b270c
johnlivingprooff <johnlivingprooff@gmail.com>
2023-09-04 19:04:15 +0200

    Updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int len, fd;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_WRONLY | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
