09a1faf2ca1f5e35866e378e87ea8bc933fddb7d
johnlivingprooff <johnlivingprooff@gmail.com>
2023-09-04 21:56:59 +0200

    Some minor updates

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define BUFFER_S 1024
 
 /**
  * main - program that copies the content of a file to another file.
@@ -8,41 +9,42 @@
  */
 int main(int ac, char **av)
 {
-	char *buffer = malloc(1024);
+	char *buffer = malloc(BUFFER_S);
 	char *file_from, *file_to;
-	int fd;
+	int ffrom, fto;
 
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
 		exit(97);
 	}
-
 	file_from = av[1];
 	file_to = av[2];
-
-	fd = open(file_from, O_RDONLY);
-	if (fd == -1 || (read(fd, buffer, 1024) == -1))
+	ffrom = open(file_from, O_RDONLY);
+	if (ffrom == -1 || (read(ffrom, buffer, BUFFER_S) == -1))
 	{
-		dprintf(2, "Error: Can't read to %s\n", av[1]);
-		close(fd);
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		close(ffrom);
 		exit(98);
 	}
-
-	fd = open(file_to, O_WRONLY | O_CREAT, 0664);
-	if (fd == -1 || (write(fd, buffer, 1024) == -1))
+	fto = open(file_to, O_WRONLY | O_CREAT, 0664);
+	if (fto == -1 || (write(fto, buffer, BUFFER_S) == -1))
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		close(fd);
+		close(ffrom);
+		close(fto);
 		exit(99);
 	}
-
-	if (close(fd) == -1)
+	if (close(ffrom) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i", fd);
+		dprintf(2, "Error: Can't close fd %i", ffrom);
+		exit(100);
+	}
+	if (close(fto) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %i", fto);
 		exit(100);
 	}
 
-	close(fd);
 	return (0);
 }
