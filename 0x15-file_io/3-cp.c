9289c3fcd6edfd8c8744955a06a07c747d5bbefc
johnlivingprooff <johnlivingprooff@gmail.com>
2023-09-04 22:49:01 +0200

    betty's got issues with this code, ove 50 lines

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,22 +20,34 @@ int main(int ac, char **av)
 	file_from = av[1];
 	file_to = av[2];
 	ffrom = open(file_from, O_RDONLY);
-	if (ffrom == -1 || (read(ffrom, buffer, BUFFER_S) == -1))
+	if (ffrom == -1)
 	{
 		dprintf(2, "Error: Can't read from %s\n", av[1]);
-		close(ffrom);
 		exit(98);
 	}
-	fto = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fto = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fto == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		close(ffrom);
+		exit(99);
+	}
 	while ((r_val = read(ffrom, buffer, BUFFER_S)) > 0)
 	{
-		if (fto == -1 || (write(fto, buffer, r_val) == -1))
+		if (write(fto, buffer, r_val) == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			close(ffrom);
 			close(fto);
 			exit(99);
 		}
+	}
+	if (r_val == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		close(ffrom);
+		close(fto);
+		exit(98);
 	}
 	if (close(ffrom) == -1)
 	{
