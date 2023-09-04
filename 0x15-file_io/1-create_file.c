8befb7e11ae8972920b436179c0e03d792625a62
johnlivingprooff <johnlivingprooff@gmail.com>
2023-09-05 00:04:54 +0200

    shh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,13 +18,15 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
+	{
 		len = strlen(text_content);
 
-	w_val = write(fd, text_content, len);
-	if (w_val == -1 || w_val < len)
-	{
-		close(fd);
-		return (-1);
+		w_val = write(fd, text_content, len);
+		if (w_val == -1 || w_val < len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
