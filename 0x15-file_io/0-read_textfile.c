d1d81c28b46bfe39ea6d577b8551e7baae04e502
johnlivingprooff <johnlivingprooff@gmail.com>
2023-09-05 23:37:26 +0200

    stderr

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	if (write(1, buf, r_val) == -1)
+	if (write(2, buf, r_val) == -1)
 	{
 		close(filep);
 		free(buf);
