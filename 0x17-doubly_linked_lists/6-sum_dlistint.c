ce4a08d3c6812c3902ec1573d2a7e6eaf3b0e251
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-12 22:25:40 +0200

    Removed whitespace in Tsk6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all 
+ * sum_dlistint - returns the sum of all
  * the data (n) of a dlistint_t linked list.
  * @head: pointer to the list
  * Return: Sum,or 0 if empty
