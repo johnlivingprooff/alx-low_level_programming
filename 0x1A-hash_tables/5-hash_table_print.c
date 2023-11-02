fe050792028ba654a70c6a8157589c45b9e9ba94
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-03 00:37:40 +0200

    print the hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hashtable
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	size_t index, i = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		printf("{}");
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+		while (node != NULL)
+		{
+			if (i > 0)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			i++;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
