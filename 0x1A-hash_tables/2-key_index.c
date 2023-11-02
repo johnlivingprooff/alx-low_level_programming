32a0afdeadddb0417009c6b627fa2c08fe78219f
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-03 00:07:57 +0200

    Update three

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,12 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	size_t result = hash_djb2(key) % size;
+	size_t result;
+	
+	if (size <= 0)
+		return (0);
+
+	result = hash_djb2(key) % size;
 
 	return (result);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: the hashtable
+ * @key: the key/identifier
+ * Return: the value, or NUll
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	size_t index;
+	hash_node_t *node;
+	
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	
+	return (NULL);
+}
