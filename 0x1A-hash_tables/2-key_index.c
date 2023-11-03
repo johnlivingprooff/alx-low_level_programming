23ad72f4ad96b640f04372209bf878798b345881
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-03 10:46:36 +0200

    removed whitespace

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	size_t result;
-	
+
 	if (size <= 0)
 		return (0);
 
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: the hashtable
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+    size_t i;
+    hash_node_t *current, *node;
+
+    if (ht == NULL)
+        return;
+
+    for (i = 0; i < ht->size; i++)
+    {
+        current = ht->array[i];
+        while (current != NULL)
+        {
+            node = current;
+            current = current->next;
+            free(node->key);
+            free(node->value);
+            free(node);
+        }
+        
+    }
+
+    free(ht->array);
+    free(ht);
+}
