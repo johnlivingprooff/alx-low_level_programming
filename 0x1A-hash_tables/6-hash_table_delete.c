6458527d27d8b2d8d474bd075e85796317c082b6
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-03 12:14:00 +0200

    fixed some indentations

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,26 +6,26 @@
 */
 void hash_table_delete(hash_table_t *ht)
 {
-    size_t i;
-    hash_node_t *current, *node;
+	size_t i;
+	hash_node_t *current, *node;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while (current != NULL)
-        {
-            node = current;
-            current = current->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-        }
-        
-    }
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			node = current;
+			current = current->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+		}
 
-    free(ht->array);
-    free(ht);
+	}
+
+	free(ht->array);
+	free(ht);
 }
